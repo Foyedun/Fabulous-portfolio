46d11189f8cc85f028f570f2a32d1ae905a7bbc7
shogooyedun <segunoyedun@frootify.tech>
2025-01-06 05:09:19 -0800

    i corrected my code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,6 @@ import Vestium from "../assets/img/vest.jpg";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import light from "../assets/img/light.PNG"
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
